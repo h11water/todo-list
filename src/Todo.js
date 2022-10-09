687303d3c7478ffd09965ea86a4c599bb9c8c070
h11 <legospacett@gmail.com>
2022-10-09 23:37:30 +0100

    separated the todo checkbox and label

--- src/Todo.js
+++ src/Todo.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import "./style.css"
+import MyTooltip from './components/MyTooltip';
+import ReactTooltip from "react-tooltip";
 
-function Todo({todo, handleMarkComplete}) {
-	function checkBoxOnChnage(){
+function Todo({ todo, handleMarkComplete }) {
+	function checkBoxOnChange() {
 		handleMarkComplete(todo.id)
 	}
 
+	function hoverDate() {
+		console.log(new Date(todo.dateAdded).toDateString())
+	}
+
 	return (
-		<div className="todoItem">
-			<label>
-				<input type="checkbox" defaultChecked = {todo.complete} onChange={checkBoxOnChnage}/>
-				{todo.name}
-			</label>
-		</div>
+		<>
+			<ReactTooltip place="top" type="dark" effect="solid" />
+			<div className="todoContainer">
+				<input type="checkbox" class="form-check-input" defaultChecked={todo.complete} onChange={checkBoxOnChange} checked={todo.complete} />
+
+				<label className="todoItem" onMouseEnter={hoverDate} data-delay-show='30' data-tip={new Date(todo.dateAdded).toDateString()}
+				onClick={checkBoxOnChange}
+				>
+					{todo.name}
+				</label>
+			</div>
+
+
+		</>
+
 	);
 }
 
